c41ed861a4bac01bfabbcfb2d1ee6bbc37d33305
Radoslaw Kopec <radek4ec@gmail.com>
2021-05-21 17:31:23 +0200

    Add api key config files

---
+++ config/corona_api.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"x-rapidapi-key": "Your api key here",
+	"x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
+	"useQueryString": true
+};
---
+++ config/country_api.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "x-rapidapi-key": "Your api key here",
+    "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+    "useQueryString": true
+};
